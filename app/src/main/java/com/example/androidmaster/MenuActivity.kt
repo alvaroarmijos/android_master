f41bc34a6218972749355475dbd22c3f1668239a
Alvaro Armijos <alvaro.armijos@zemoga.com>
2023-04-17 12:32:22 -0500

    TODO App finished

--- app/src/main/java/com/example/androidmaster/MenuActivity.kt
+++ app/src/main/java/com/example/androidmaster/MenuActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import com.example.androidmaster.firstapp.FirstAppActivity
 import com.example.androidmaster.imccalculator.ImcCalculatorActivity
+import com.example.androidmaster.todoapp.TodoActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +15,15 @@ class MenuActivity : AppCompatActivity() {
 
         val btnSaludApp = findViewById<Button>(R.id.btnSaludapp)
         val btnIMCApp = findViewById<Button>(R.id.btnIMCApp)
+        val btnTodoApp = findViewById<Button>(R.id.btnTodoApp)
         btnSaludApp.setOnClickListener { navigateToSaludApp() }
         btnIMCApp.setOnClickListener { navigateToIMCApp() }
+        btnTodoApp.setOnClickListener { navigateToTodoApp() }
+    }
+
+    private fun navigateToTodoApp() {
+        val intent = Intent(this, TodoActivity::class.java)
+        startActivity(intent)
     }
 
     private fun navigateToIMCApp() {
---
+++ app/src/main/java/com/example/androidmaster/todoapp/TodoActivity.kt
@@ -0,0 +1,113 @@
+package com.example.androidmaster.todoapp
+
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androidmaster.R
+import com.example.androidmaster.todoapp.TaskCategory.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class TodoActivity : AppCompatActivity() {
+
+    private val categories = listOf<TaskCategory>(
+        Business,
+        Personal,
+        Other,
+    )
+
+    private val tasks = mutableListOf<Task>(
+        Task("PruebaB", Business),
+        Task("PruebaP", Personal),
+        Task("PruebaO", Other),
+    )
+
+    private lateinit var rvCategories: RecyclerView
+    private lateinit var categoriesAdapter: CategoriesAdapter
+
+    private lateinit var rvTasks: RecyclerView
+    private lateinit var tasksAdapter: TaskAdapter
+
+    private lateinit var fabAddTask: FloatingActionButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todo)
+        initComponent()
+        initUi()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        fabAddTask.setOnClickListener { showDialog() }
+    }
+
+    private fun showDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_task)
+
+        val btnAddTask: Button = dialog.findViewById(R.id.btnAddTask)
+        val etText: EditText = dialog.findViewById(R.id.etTask)
+        val rgCategories: RadioGroup = dialog.findViewById(R.id.rgCategories)
+
+        btnAddTask.setOnClickListener {
+            val currentTask = etText.text.toString()
+            if (currentTask.isNotEmpty()) {
+                val selectedId = rgCategories.checkedRadioButtonId
+                val selectedRadioButton: RadioButton = rgCategories.findViewById(selectedId)
+                val curretCategory: TaskCategory = when (selectedRadioButton.text) {
+                    getString(R.string.todo_dialog_category_business) -> Business
+                    getString(R.string.todo_dialog_category_personal) -> Personal
+                    else -> Other
+                }
+
+                tasks.add(Task(currentTask, curretCategory))
+                updateTasks()
+                dialog.hide()
+            }
+
+
+        }
+        dialog.show()
+    }
+
+    private fun initUi() {
+        categoriesAdapter = CategoriesAdapter(categories) {updateCategories(it)}
+        rvCategories.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rvCategories.adapter = categoriesAdapter
+
+        tasksAdapter = TaskAdapter(tasks) { onItemSelected(it) }
+        rvTasks.layoutManager = LinearLayoutManager(this)
+        rvTasks.adapter = tasksAdapter
+    }
+
+    private fun initComponent() {
+        rvCategories = findViewById(R.id.rvCategories)
+        rvTasks = findViewById(R.id.rvTasks)
+        fabAddTask = findViewById(R.id.fabAddTask)
+    }
+
+    private fun onItemSelected(position: Int) {
+        tasks[position].isSelected = !tasks[position].isSelected
+        updateTasks()
+    }
+
+    private fun updateCategories(position: Int){
+        categories[position].isSelected = !categories[position].isSelected
+        categoriesAdapter.notifyItemChanged(position)
+        updateTasks()
+    }
+
+    private fun updateTasks() {
+        val selectedCategories:List<TaskCategory> = categories.filter { it.isSelected }
+        val newTasks = tasks.filter { selectedCategories.contains(it.category) }
+        tasksAdapter.tasks = newTasks
+        tasksAdapter.notifyDataSetChanged()
+    }
+}
